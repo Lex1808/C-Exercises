9b49fe5fad8ca18f1cf7621a584a5ef6ba1dcc05
Oleksandr <112832902+Lex1808@users.noreply.github.com>
2024-10-31 19:57:56 +0200

    Update stackADT.h

--- Other/StackADT/stackADT.h
+++ Other/StackADT/stackADT.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 
+typedef int Item;
 typedef struct struct_type *Stack;
 
 Stack create(void);
@@ -10,7 +11,7 @@ void destroy(Stack s);
 void make_empty(Stack s);
 bool is_empty(Stack s);
 bool is_full(Stack s);
-void push(Stack s, int i);
-int pop(Stack s);
+void push(Stack s, Item i);
+Item pop(Stack s);
 
 #endif
